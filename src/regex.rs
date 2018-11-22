65c1a4047cbf44c772b85a9bd21578d6ea5ff2fa
HeroicKatora <andreas.molzer@gmx.de>
2018-11-22 15:44:55 +0100

    Simple regex construction

--- src/regex.rs
+++ src/regex.rs
@@ -2,7 +2,23 @@ use super::Alphabet;
 use super::nfa::Nfa;
 
 /// Represents regular expressions over some finite alphabet.
-pub struct Regex<A: Alphabet>(A);
+///
+/// Optimizes storage and construction for reoccurring subexpressions to allow
+/// polynomial time conversion from NFA. You can create (and keep) handles on 
+/// subexpressions, then evaluate as if those subexpressions were at the root.
+/// This relationship forms an acyclic graph.
+pub struct Regex<A: Alphabet> {
+    subs: Vec<Op<A>>,
+}
+
+pub struct Handle(usize);
+
+pub enum Op<A: Alphabet> {
+    Match(A),
+    Star(Handle),
+    Or(Handle, Handle),
+    Concat(Handle, Handle),
+}
 
 impl<A: Alphabet> Regex<A> {
     /// Idea:
@@ -10,5 +26,28 @@ impl<A: Alphabet> Regex<A> {
     /// Is like a regex-labeled nfa with only one final state.
     pub fn to_nfa(self) -> Nfa<A> {
         unimplemented!()
+    }
+
+    /// Push a new operation as the regex root.
+    ///
+    /// It is not required that all regex states are reachable afterwards but all
+    /// handles must point to existing operations. Returns a handle on the newly
+    /// inserted operation.
+    pub fn push(&mut self, op: Op<A>) -> Handle {
+        match op {
+            Op::Match(_) => (),
+            Op::Star(Handle(i)) => assert!(i < self.subs.len()),
+            Op::Or(Handle(i), Handle(j)) => assert!(i < self.subs.len() && j < self.subs.len()),
+            Op::Concat(Handle(i), Handle(j)) => assert!(i < self.subs.len() && j < self.subs.len()),
+        }
+
+        let handle = Handle(self.subs.len());
+        self.subs.push(op);
+        handle
+    }
+
+    /// Get a root to the regex.
+    pub fn root(&self) -> Option<Handle> {
+        self.subs.len().checked_sub(1).map(Handle)
     }
 }
